64117ae9cc30070656a42502b16f9174414883c4
Hassan Noorani <hanoorani1@gmail.com>
2023-06-19 19:38:59 -0400

    Added second activity, switch, image, etc

--- app/src/main/java/hassan/noorani/midtermapplication/MainActivity.java
+++ app/src/main/java/hassan/noorani/midtermapplication/MainActivity.java
@@ -2,13 +2,46 @@ package hassan.noorani.midtermapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch OnOffSwitch = findViewById((R.id.Has5518switch1));
+        ImageButton imageButton = findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        OnOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String message;
+                if (isChecked) {message = "John ON";} else {message = "John OFF";}
+
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
+            }
+
+        });
+
+
     }
 }
