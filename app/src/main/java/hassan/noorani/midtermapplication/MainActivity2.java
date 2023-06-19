e67eaecd7fe2146a0df11716f8ff07c64346e528
Hassan Noorani <hanoorani1@gmail.com>
2023-06-19 19:45:21 -0400

    Added Back Button

--- app/src/main/java/hassan/noorani/midtermapplication/MainActivity2.java
+++ app/src/main/java/hassan/noorani/midtermapplication/MainActivity2.java
@@ -3,6 +3,7 @@ package hassan.noorani.midtermapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed(); // Go back to the previous activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
